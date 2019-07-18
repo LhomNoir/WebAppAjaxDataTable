fb904cd9cb04b8448aafbfddb48d954cc23aab17
DEMABOU <citoyenlamda@gmail.com>
2019-07-18 19:50:43 +0200

    first save

---
+++ WebAppAjaxDataTable/Controllers/EmployeeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppAjaxDataTable.Models;
+using WebAppAjaxDataTable.Repositories;
+
+namespace WebAppAjaxDataTable.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee  
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        private EmployeeRepository EmployeeRepository;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult GetList()
+        {
+            int start = Convert.ToInt32(Request["start"]);
+            int length = Convert.ToInt32(Request["length"]);
+            string searchVale = Request["search[valie]"];
+            string srotColumnName = Request["cloumn[" + Request["order[0][column]"] + "][name]"];
+            string sortDirection = Request["order[0][dir]"];
+
+
+            EmployeeRepository = new EmployeeRepository();
+            var employeeList = EmployeeRepository.PopulateEmployeeList();
+
+            return Json(new { data = employeeList }, JsonRequestBehavior.AllowGet);
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult AddOrEdit(int id)
+        {
+            if (id == 0)
+            {
+                return View(new Employee());
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ WebAppAjaxDataTable/Repositories/EmployeeRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebAppAjaxDataTable.Models;
+
+namespace WebAppAjaxDataTable.Repositories
+{
+    public class EmployeeRepository 
+    {        
+        public IList<Employee> PopulateEmployeeList()
+        {
+            var employees = new List<Employee>
+            {
+                new Employee { Name = "Jamy", Position = "Salary", Office = "London", Age = 31 },
+                new Employee { Name = "Carol", Position = "Chief officer", Office = "London", Age = 31 },
+                new Employee { Name = "Mary", Position = "Manager", Office = "Paris", Age = 31 },
+                new Employee { Name = "Jimy", Position = "Salary", Office = "London", Age = 31 },
+                new Employee { Name = "Patrik", Position = "Salary", Office = "London", Age = 31 },
+                new Employee { Name = "Obama", Position = "Salary", Office = "London", Age = 31 },
+                new Employee { Name = "Karl", Position = "Salary", Office = "London", Age = 31 },
+
+                new Employee { Name = "Jamy", Position = "Salary", Office = "London", Age = 31 },
+                new Employee { Name = "Carol", Position = "Chief officer", Office = "London", Age = 31 },
+                new Employee { Name = "Mary", Position = "Manager", Office = "Paris", Age = 31 },
+                new Employee { Name = "Jimy", Position = "Salary", Office = "London", Age = 31 },
+                new Employee { Name = "Patrik", Position = "Salary", Office = "London", Age = 31 },
+                new Employee { Name = "Obama", Position = "Salary", Office = "London", Age = 31 },
+                new Employee { Name = "Karl", Position = "Salary", Office = "London", Age = 31 }
+            };
+
+            return employees;
+        }
+    }
+}
